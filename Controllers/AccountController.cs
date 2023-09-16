39e052ac3e7c83e902e97bdcbcc86213cc61ca56
Harsh1848 <harsh.hp7999@gmail.com>
2023-09-16 18:37:48 -0400

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using GSL.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GSL.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UserRegistrationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Use Firebase SDK to register the user and save other details in Firestore
+            }
+            return RedirectToAction("Login");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Use Firebase SDK to authenticate the user
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
---
+++ Models/UserRegistrationModel.cs
@@ -0,0 +1,13 @@
+﻿namespace GSL.Models
+{
+    public class UserRegistrationModel
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string UserType { get; set; } // Customer or SalesProvider
+        public string Password { get; set; }
+    }
+
+}
